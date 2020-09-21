17dd6264a230d4e456de7cb3f219958471470dad
Miraclx <omiraculous@gmail.com>
2020-09-21 22:16:24 +0100

    Document the function generating the pascals triangle

--- src/lib.rs
+++ src/lib.rs
@@ -344,6 +344,42 @@ where
     })
 }
 
+/// Returns a vector of vectors of representing n layers of a binomial triangle
+///
+/// # Examples
+///
+/// ```
+/// use math::pascals;
+///
+/// assert_eq!(
+///     pascals(5),
+///     vec![
+///         vec![1],
+///         vec![1, 1],
+///         vec![1, 2, 1],
+///         vec![1, 3, 3, 1],
+///         vec![1, 4, 6, 4, 1],
+///     ]
+/// );
+/// assert_eq!(
+///     pascals(10),
+///     vec![
+///         vec![1],
+///         vec![1, 1],
+///         vec![1, 2, 1],
+///         vec![1, 3, 3, 1],
+///         vec![1, 4, 6, 4, 1],
+///         vec![1, 5, 10, 10, 5, 1],
+///         vec![1, 6, 15, 20, 15, 6, 1],
+///         vec![1, 7, 21, 35, 35, 21, 7, 1],
+///         vec![1, 8, 28, 56, 70, 56, 28, 8, 1],
+///         vec![1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+///     ]
+/// );
+/// ```
+///
+/// [`nCr`]: ./fn.combination.html
+
 pub fn pascals(n: u32) -> Vec<Vec<u32>> {
     let mut result = vec![vec![1], vec![1, 1]];
     for row in 2..n as usize {
