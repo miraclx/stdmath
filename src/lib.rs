b1fbb12a1548ee0c812681ddf982df1084b1cb50
Miraclx <omiraculous@gmail.com>
2020-09-21 21:49:14 +0100

    Implement product function

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,16 @@ where
 {
     (start..=end).map(func).sum()
 }
+
+#[inline]
+pub fn product<T, R>(min: T, max: T, func: impl Fn(T) -> R) -> R
+where
+    T: std::iter::Step,
+    R: std::iter::Product,
+{
+    (min..=max).map(func).product()
+}
+
 #[cfg(test)]
 mod tests {
     use crate::*;
@@ -17,5 +27,12 @@ mod tests {
         assert_eq!(sigma(1, 1, |x| x), 1);
         assert_eq!(sigma(1, 10, |x| x), 55);
         assert_eq!(sigma(1, 10, |x| u32::pow(x, 2)), 385);
+    }
+    #[test]
+    fn test_product() {
+        assert_eq!(product(0, 0, |x| x), 0);
+        assert_eq!(product(1, 1, |x| x), 1);
+        assert_eq!(product(1, 10, |x| x), 3628800);
+        assert_eq!(product(1, 10, |x| u64::pow(x, 2)), 13168189440000);
     }
 }
