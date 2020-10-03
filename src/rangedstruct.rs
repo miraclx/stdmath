f8f83e3dfd74c5e4055cdc84fd1b1fa867be8ef6
Miraclx <omiraculous@gmail.com>
2020-10-03 15:48:29 +0100

    Document div_mixed_compute test

--- src/rangedstruct.rs
+++ src/rangedstruct.rs
@@ -358,6 +358,9 @@ mod tests {
     }
     #[test]
     fn div_mixed_compute() {
+        // (1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10) / (6 * 7 * 8 * 9 * 10 * 11 * 12 * 13 * 14 * 15)
+        //  = (1 * 2 * 3 * 4 * 5) / (11 * 12 * 13 * 14 * 15)
+        //  = 0.000333000333000333
         let func = |x| x as f64;
         let val1 = RangedStruct::from_normal(1..=10, func);
         let val2 = RangedStruct::from_normal(6..=15, func);
