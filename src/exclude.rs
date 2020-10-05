eadf50e6561f642bd7e04c2d1a9c82eb4317ffb2
Miraclx <omiraculous@gmail.com>
2020-10-05 12:52:23 +0100

    Remove OverflowedIterator and OverflowStatus: now unneccessary after HEAD~1

--- src/exclude.rs
+++ src/exclude.rs
@@ -1,53 +1,4 @@
-use std::{
-    collections::{hash_map::IntoIter, HashMap},
-    hash::Hash,
-};
-
-pub struct OverflowedIterator<I, T> {
-    inner: I,
-    cursor: Option<(T, usize)>,
-}
-
-impl<I: Iterator, T> OverflowedIterator<I, T> {
-    pub fn new<F>(map: F) -> Self
-    where
-        F: IntoIterator<Item = I::Item, IntoIter = I>,
-        I: Iterator<Item = (T, usize)>,
-    {
-        OverflowedIterator {
-            inner: map.into_iter(),
-            cursor: None,
-        }
-    }
-}
-
-impl<I: Iterator, T> Iterator for OverflowedIterator<I, T>
-where
-    T: Copy + Eq,
-    I: Iterator<Item = (T, usize)>,
-{
-    type Item = T;
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            if let Some((key, ref mut val)) = self.cursor {
-                *val -= 1;
-                if *val == 0 {
-                    self.cursor = self.inner.next();
-                }
-                return Some(key);
-            }
-            self.cursor = Some(self.inner.next()?);
-        }
-    }
-}
-
-pub enum OverflowStatus<T, R> {
-    Excluded,
-    Included(
-        fn(T) -> R,
-        Option<OverflowedIterator<IntoIter<T, usize>, T>>,
-    ),
-}
+use std::{collections::HashMap, hash::Hash};
 
 #[derive(Clone)]
 pub enum OverflowState<T, R> {
@@ -196,14 +147,6 @@ mod tests {
             (1..=10).exclude(3..6).exclude(7..10).collect::<Vec<_>>(),
             vec![1, 2, 6, 10]
         );
-    }
-    #[test]
-    fn test_counted_iter() {
-        use std::collections::HashMap;
-        let list: HashMap<&str, usize> = [("a", 1), ("b", 2), ("c", 3)].iter().copied().collect();
-        let mut items = OverflowedIterator::new(list).collect::<Vec<_>>();
-        items.sort();
-        assert_eq!(items, vec!["a", "b", "b", "c", "c", "c"]);
     }
     #[test]
     fn exclusion_overflow() {
