ff58138a6196b99c984e91d5f76718b8557c2b57
Miraclx <omiraculous@gmail.com>
2020-10-26 04:31:59 +0100

    Add base feature implementation for maintaining order with exclusion overflow

--- src/exclude.rs
+++ src/exclude.rs
@@ -1,4 +1,16 @@
-use std::{collections::HashMap, hash::Hash};
+#[cfg(feature = "order")]
+use std::{collections::hash_map::DefaultHasher, hash::Hasher};
+use std::{
+    collections::{BTreeMap, HashMap},
+    hash::Hash,
+};
+
+#[cfg(feature = "order")]
+fn hash<T: Hash>(val: &T) -> u64 {
+    let mut state = DefaultHasher::new();
+    val.hash(&mut state);
+    state.finish()
+}
 
 #[derive(Clone)]
 pub enum OverflowState<T, R> {
@@ -6,15 +18,28 @@ pub enum OverflowState<T, R> {
     Included(fn(T) -> R),
 }
 
+#[cfg(feature = "order")]
+#[derive(Clone, Default)]
+struct DictionaryEntry<T> {
+    cursor: usize,
+    slots: Vec<(usize, T)>,
+}
+
 #[derive(Clone)]
 pub struct ExcludedIterator<B, C: Iterator, R> {
     base: B,
+    #[cfg(feature = "order")]
+    ctrl: HashMap<u64, DictionaryEntry<C::Item>>,
+    #[cfg(feature = "order")]
+    hash_entries: BTreeMap<usize, u64>,
+    #[cfg(not(feature = "order"))]
     ctrl: HashMap<C::Item, usize>,
     transformer: fn(C::Item) -> R,
     overflow: OverflowState<C::Item, R>,
 }
 
 impl<B, C: Iterator, R> ExcludedIterator<B, C, R> {
+    #[cfg(not(feature = "order"))]
     pub fn new(base: B, ctrl: C) -> Self
     where
         C: Iterator<Item = R>,
@@ -30,10 +55,45 @@ impl<B, C: Iterator, R> ExcludedIterator<B, C, R> {
             overflow: OverflowState::Excluded,
         }
     }
+    #[cfg(feature = "order")]
+    pub fn new(base: B, ctrl: C) -> Self
+    where
+        C: Iterator<Item = R>,
+        C::Item: Eq + Hash,
+    {
+        let mut _ctrl: HashMap<u64, DictionaryEntry<C::Item>> = HashMap::new();
+        let hash_entries = ctrl
+            .enumerate()
+            .map(|(index, item)| {
+                let hash = hash(&item);
+
+                _ctrl
+                    .entry(hash)
+                    .or_insert_with(|| DictionaryEntry {
+                        cursor: 0,
+                        slots: vec![],
+                    })
+                    .slots
+                    .push((index, item));
+
+                (index, hash)
+            })
+            .collect();
+        ExcludedIterator {
+            base,
+            ctrl: _ctrl,
+            hash_entries,
+            #[inline]
+            transformer: |x| x,
+            overflow: OverflowState::Excluded,
+        }
+    }
     pub fn with_transformer<V>(self, transform: fn(C::Item) -> V) -> ExcludedIterator<B, C, V> {
         ExcludedIterator {
             base: self.base,
             ctrl: self.ctrl,
+            #[cfg(feature = "order")]
+            hash_entries: self.hash_entries,
             #[inline]
             transformer: transform,
             overflow: OverflowState::Excluded,
@@ -69,6 +129,7 @@ where
     T: Clone + Eq + Hash,
 {
     type Item = R;
+    #[cfg(not(feature = "order"))]
     fn next(&mut self) -> Option<Self::Item> {
         loop {
             match self.base.next() {
@@ -99,6 +160,53 @@ where
                             self.ctrl.remove(&entry);
                         }
                         return Some(handle(entry));
+                    }
+                },
+            };
+        }
+    }
+    #[cfg(feature = "order")]
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            match self.base.next() {
+                Some(val) => {
+                    if self.ctrl.is_empty() {
+                        return Some((self.transformer)(val));
+                    } else {
+                        let hash = hash(&val);
+                        match self.ctrl.get_mut(&hash) {
+                            Some(entry) => {
+                                let (index, val_) = entry
+                                    .slots
+                                    .get(entry.cursor)
+                                    .expect("entry slot cursor out of bounds");
+                                if val != *val_ {
+                                    return Some((self.transformer)(val));
+                                }
+                                self.hash_entries.remove(index);
+                                entry.cursor += 1;
+                                if entry.cursor == entry.slots.len() {
+                                    self.ctrl.remove(&hash);
+                                }
+                            }
+                            None => return Some((self.transformer)(val)),
+                        }
+                    }
+                }
+                None => match self.overflow {
+                    OverflowState::Excluded => return None,
+                    OverflowState::Included(handle) => {
+                        let index = *self.hash_entries.keys().next()?;
+                        let hash = self.hash_entries.remove(&index)?;
+                        let DictionaryEntry { slots, cursor } = self
+                            .ctrl
+                            .get_mut(&hash)
+                            .expect("can't find hash entry within dictionary");
+                        let (_, val) = slots.pop()?;
+                        if *cursor == slots.len() {
+                            self.ctrl.remove(&hash);
+                        }
+                        return Some(handle(val));
                     }
                 },
             };
