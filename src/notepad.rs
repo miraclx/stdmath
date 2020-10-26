ebc4f4d3eb92e358a6b183a49076c28e6e627653
Miraclx <omiraculous@gmail.com>
2020-10-26 23:33:19 +0100

    Move Type declaration from core to notepad

--- src/notepad.rs
+++ src/notepad.rs
@@ -1,14 +1,208 @@
-use super::{
-    core::{Flippable, Type},
-    exclude::ExcludedIteratorExt,
-    One, Zero,
-};
+use super::{exclude::ExcludedIteratorExt, One, Zero};
 use std::{
     any::Any,
     cmp::{Eq, Ordering, PartialEq},
     fmt::{self, Debug, Write},
     hash::{Hash, Hasher},
 };
+
+#[derive(Eq, PartialEq, Copy, Clone, Hash, Debug, Ord, PartialOrd)]
+/// Provide a means to represent the state of a value. Normal or Reciprocal.
+pub enum Type<T> {
+    /// context        | value | identity | indirect identity
+    /// -------------- | ----- | -------- | -----------------
+    /// multiplicative |  `3`  | `3 * 1`  | `3 / 1`
+    /// additive       |  `3`  | `3 + 0`  | `3 - 0`
+    Normal(T),
+    /// context        | value | identity | indirect identity
+    /// -------------- | ----- | -------- | -----------------
+    /// multiplicative |  `3`  | `1 / 3`  | `1 * 3^-1`
+    /// additive       |  `3`  | `0 - 3`  | `0 + -3`
+    Inverse(T),
+}
+
+impl<T> Type<T> {
+    /// Inverts the state of the Type
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use stdmath::core::Typ{mul, sum, Context, Type}e;
+    /// #
+    /// let val = Type::Normal(10);
+    /// let inv = val.flip();
+    /// assert_eq!(inv, Type::Inverse(10));
+    ///
+    /// let val = Type::Inverse(30);
+    /// let inv = val.flip();
+    /// assert_eq!(inv, Type::Normal(30));
+    /// ```
+    pub fn flip(self) -> Self {
+        match self {
+            Type::Normal(val) => Type::Inverse(val),
+            Type::Inverse(val) => Type::Normal(val),
+        }
+    }
+    /// Matches the type variant, returning true if self is [`Type::Inverse`](#variant.Inverse)
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use stdmath::core::Type;
+    /// #
+    /// let val = Type::Normal(());
+    /// assert_eq!(val.is_inverted(), false);
+    ///
+    /// let val = Type::Inverse(());
+    /// assert_eq!(val.is_inverted(), true);
+    /// ```
+    pub fn is_inverted(&self) -> bool {
+        if let Type::Inverse(_) = self {
+            return true;
+        };
+        false
+    }
+    /// Extracts the value contained in a variant of `Type`
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use stdmath::core::Type;
+    /// #
+    /// let val = Type::Normal(21);
+    /// assert_eq!(val.unwrap(), 21);
+    ///
+    /// let val = Type::Inverse("hello");
+    /// assert_eq!(val.unwrap(), "hello");
+    /// ```
+    pub fn unwrap(self) -> T {
+        match self {
+            Type::Normal(val) => val,
+            Type::Inverse(val) => val,
+        }
+    }
+    /// Transform the value within the type variant, returning the result contained within same type variant.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use stdmath::core::Type;
+    /// #
+    /// let val = Type::Normal(533);
+    /// assert_eq!(val.map(|num| num - 33), Type::Normal(500));
+    ///
+    /// let val = Type::Inverse(10);
+    /// assert_eq!(val.map(|num| num * 20), Type::Inverse(200));
+    /// ```
+    pub fn map<P: Fn(T) -> R, R>(self, func: P) -> Type<R> {
+        match self {
+            Type::Normal(val) => Type::Normal(func(val)),
+            Type::Inverse(val) => Type::Inverse(func(val)),
+        }
+    }
+    /// Converts from a `&Type<T>` to a `Type<&T>`
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use stdmath::core::Type;
+    /// #
+    /// let val = Type::Normal(100);
+    /// assert_eq!(val.as_ref().map(|val| val * 2), Type::Normal(200));
+    ///
+    /// let val = Type::Inverse("Hello");
+    /// assert_eq!(val.as_ref().map(|txt| txt.len()), Type::Inverse(5));
+    /// ```
+    pub fn as_ref(&self) -> Type<&T> {
+        match self {
+            Type::Normal(val) => Type::Normal(val),
+            Type::Inverse(val) => Type::Inverse(val),
+        }
+    }
+    /// Converts from a `&mut Type<T>` to a `Type<&mut T>`
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use stdmath::core::Type;
+    /// #
+    /// let mut text = Type::Normal("Hello".to_string());
+    /// text.as_mut().map(|s| s.push_str(", World!"));
+    /// assert_eq!(text.as_ref().map(|s| s.as_str()), Type::Normal("Hello, World!"));
+    ///
+    /// let mut val = Type::Inverse(33);
+    /// val.as_mut().map(|v| *v += 20);
+    /// assert_eq!(val, Type::Inverse(53));
+    /// ```
+    pub fn as_mut(&mut self) -> Type<&mut T> {
+        match self {
+            Type::Normal(val) => Type::Normal(val),
+            Type::Inverse(val) => Type::Inverse(val),
+        }
+    }
+}
+
+/// Provide a means to convert an iterator of `T` to one of `Type<T>`
+///
+/// # Examples
+/// ```
+/// # use stdmath::core::Type;
+/// #
+/// let vals = Type::Normal(1..=5).collect::<Vec<_>>();
+/// assert_eq!(vals, vec![
+///     Type::Normal(1),
+///     Type::Normal(2),
+///     Type::Normal(3),
+///     Type::Normal(4),
+///     Type::Normal(5)
+/// ]);
+///
+/// let vals = Type::Inverse(6..=10).collect::<Vec<_>>();
+/// assert_eq!(vals, vec![
+///     Type::Inverse(6),
+///     Type::Inverse(7),
+///     Type::Inverse(8),
+///     Type::Inverse(9),
+///     Type::Inverse(10)
+/// ]);
+/// ```
+impl<I: Iterator<Item = T>, T> Iterator for Type<I> {
+    type Item = Type<T>;
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.as_mut().map(|item| item.next()) {
+            Type::Normal(result) => Some(Type::Normal(result?)),
+            Type::Inverse(result) => Some(Type::Inverse(result?)),
+        }
+    }
+}
+
+#[derive(Clone)]
+/// Provide a means to convert an iterator of `Type`s to their inverse variants
+/// e.g `[Type::Normal(x), Type::Flipped(y)]` becomes `[Type::Flipped(x), Type::Normal(y)]`
+pub struct FlippedIteratorOfTypes<I> {
+    inner: I,
+}
+
+impl<I: Iterator<Item = Type<T>>, T> Iterator for FlippedIteratorOfTypes<I> {
+    type Item = Type<T>;
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(self.inner.next()?.flip())
+    }
+}
+
+pub trait Flippable<I> {
+    fn flip(self) -> FlippedIteratorOfTypes<I>;
+}
+
+// adds the .flip() method to all iterators whose items are Type<T>
+impl<I, T> Flippable<I> for I
+where
+    I: Iterator<Item = Type<T>>,
+{
+    fn flip(self) -> FlippedIteratorOfTypes<I> {
+        FlippedIteratorOfTypes { inner: self }
+    }
+}
 
 pub trait Simplify {
     fn simplify(&self, file: &mut dyn Write) -> std::fmt::Result;
@@ -660,6 +854,90 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[test]
+    fn type_flip() {
+        let ty = Type::Normal(());
+        assert_eq!(ty, Type::Normal(()));
+        assert_ne!(ty, Type::Inverse(()));
+        assert_eq!(ty.flip(), Type::Inverse(()));
+
+        let ty = Type::Inverse(());
+        assert_eq!(ty, Type::Inverse(()));
+        assert_ne!(ty, Type::Normal(()));
+        assert_eq!(ty.flip(), Type::Normal(()));
+    }
+    #[test]
+    fn type_val_map() {
+        let ty = Type::Normal(10);
+        assert_eq!(ty, Type::Normal(10));
+        assert_eq!(ty.map(|val| val * 2), Type::Normal(20));
+    }
+    #[test]
+    fn type_val_unwrap() {
+        let ty = Type::Normal(true);
+        assert_eq!(ty.unwrap(), true);
+        assert_ne!(ty.unwrap(), false);
+    }
+    #[test]
+    fn type_val_check_state() {
+        let ty = Type::Normal(true);
+        assert_eq!(ty.is_inverted(), false);
+        assert_ne!(ty.is_inverted(), true);
+
+        let ty = Type::Inverse(true);
+        assert_eq!(ty.is_inverted(), true);
+        assert_ne!(ty.is_inverted(), false);
+
+        let ty = Type::Inverse(false).flip().map(|state| !state);
+        assert_eq!(ty.is_inverted(), false);
+        assert_eq!(ty, Type::Normal(true));
+    }
+    #[test]
+    fn iter_type_flip() {
+        // flip an iterator of types
+        assert_eq!(
+            vec![
+                Type::Normal(5),
+                Type::Inverse(10),
+                Type::Inverse(15),
+                Type::Normal(20)
+            ]
+            .into_iter()
+            .flip()
+            .collect::<Vec<_>>(),
+            vec![
+                Type::Inverse(5),
+                Type::Normal(10),
+                Type::Normal(15),
+                Type::Inverse(20)
+            ]
+        );
+    }
+    #[test]
+    fn convert_iter_t_to_iter_type_t() {
+        assert_eq!(
+            Type::Normal(1..=6).collect::<Vec<_>>(),
+            vec![
+                Type::Normal(1),
+                Type::Normal(2),
+                Type::Normal(3),
+                Type::Normal(4),
+                Type::Normal(5),
+                Type::Normal(6)
+            ]
+        );
+
+        assert_eq!(
+            Type::Normal(3..=7).flip().collect::<Vec<_>>(),
+            vec![
+                Type::Inverse(3),
+                Type::Inverse(4),
+                Type::Inverse(5),
+                Type::Inverse(6),
+                Type::Inverse(7)
+            ]
+        );
+    }
     #[test]
     fn basic_sum_repr() {
         let val = sum(Type::Normal(3..=6));
