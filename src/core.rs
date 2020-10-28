3259e871dfd22eac574f34980175e22bbd033a74
Miraclx <omiraculous@gmail.com>
2020-10-28 16:28:54 +0100

    Put Simplify::repr inline attr after documentation

--- src/core.rs
+++ src/core.rs
@@ -305,7 +305,6 @@ pub trait Simplify {
     /// assert_eq!(file, "50".to_string());
     /// ```
     fn simplify(&self, file: &mut dyn Write) -> std::fmt::Result;
-    #[inline]
     /// This helper method serializes `self` into a [`String`](std::string::String).
     ///
     /// # Examples
@@ -317,6 +316,7 @@ pub trait Simplify {
     /// 50_u8.simplify(&mut file).expect("failed to simplify");
     /// assert_eq!(file, "50".to_string());
     /// ```
+    #[inline]
     fn repr(&self) -> Result<String, std::fmt::Error> {
         let mut file = String::new();
         Simplify::simplify(self, &mut file)?;
