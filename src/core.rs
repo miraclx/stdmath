c29ff1a66208f1a829637a47900b83a88d9f4f7a
Miraclx <omiraculous@gmail.com>
2020-11-04 13:44:04 +0100

    Reimpl Debug on Trans..Value include the F type_id

--- src/core.rs
+++ src/core.rs
@@ -1329,10 +1329,13 @@ impl<T: Hash, F: 'static> Hash for TransformedValue<T, F> {
     }
 }
 
-impl<T: Debug, F> Debug for TransformedValue<T, F> {
+impl<T: Debug, F: 'static> Debug for TransformedValue<T, F> {
     #[inline]
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_tuple("TransformedValue").field(&self.0).finish()
+        f.debug_tuple("TransformedValue")
+            .field(&self.0)
+            .field(&self.1.type_id())
+            .finish()
     }
 }
 
